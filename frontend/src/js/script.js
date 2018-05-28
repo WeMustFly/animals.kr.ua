20f18b176b21408591c88c7302adb33e108b4590
Sergey Chuprina <sergey.chuprina@hyuna.bb>
2018-05-28 17:25:12 +0300

    card edited

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,5 +1,14 @@
 'use strict';
 
 (() => {
-	
+	var captions = document.querySelectorAll('.caption p');
+	captions.forEach(el=>{
+		if(el.innerText.length < 50) return;
+
+		var text = el.innerText.substring(0, 40) + '...';
+
+		el.innerText = text;
+
+		console.log(text);
+	});
 })();
