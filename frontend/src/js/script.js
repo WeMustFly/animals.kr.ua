e2b54498e60d7780b7a4f7ecb0c49cf58aff8e31
Sergey Chuprina <sergey.chuprina@hyuna.bb>
2018-05-29 11:35:45 +0300

    index.html <p> in links

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -4,11 +4,8 @@
 	var captions = document.querySelectorAll('.caption p');
 	captions.forEach(el=>{
 		if(el.innerText.length < 50) return;
-
 		var text = el.innerText.substring(0, 40) + '...';
-
 		el.innerText = text;
-		
 	});
 
 	var menu = document.getElementById('mobileMenu');
